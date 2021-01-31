37a4bdefc087140811cfdad64a3700a7ab1d8db2
carolinamedina26 <73404764+carolinamedina26@users.noreply.github.com>
2021-01-31 15:49:07 -0500

    Add files via upload

--- Earthquakes_Challenge/static/js/challenge_logic.js
+++ Earthquakes_Challenge/static/js/challenge_logic.js
@@ -16,7 +16,7 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
 });
 
 // We create the third tile layer that will be the background of our map.
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
@@ -33,7 +33,7 @@ let map = L.map('mapid', {
 let baseMaps = {
   "Streets": streets,
   "SatelliteStreets": satelliteStreets,
-  "light": light
+  "dark": dark
 };
 
 // 1. Add a 2nd layer group for the tectonic plate data.
